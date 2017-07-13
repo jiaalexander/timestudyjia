7d45f1036cbd96c8c6e8fbcaa46c7cb462277f5b
anj1 <anj1@dane-test.had.dnsops.gov>
2017-07-13 16:49:22 -0400

    filtered out plots with offsets only < 3

--- genplotspage.py
+++ genplotspage.py
@@ -16,53 +16,58 @@ def mysql_connect(passwd):
     conn.cursor().execute("set time_zone = '+00:00'")
     return conn
 
-def create_timeseries(conn, savefolder):
+def create_timeseries(conn, img_dir):
     c = conn.cursor()
     cmd = "select host from hosts where usg=1"
     c.execute(cmd)
     hosts = [row[0] for row in c.fetchall()]
+    rev_hosts = sorted([".".join(addr.split(".")[::-1]) for addr in hosts])
+    sorted_hosts = [".".join(addr.split(".")[::-1]) for addr in rev_hosts]
     cmd = "select qdatetime,ipaddr,offset from times "
-    outfile = open("timeseries.txt", 'w')
-    print("Number of hosts: "+str(len(hosts)))
-    for host in hosts:
-        c.execute("%swhere host='%s'" % (cmd,host))
-        outfile.write(host+'\n')
-        for row in c.fetchall():
-            outfile.write("%s,\t%s,\t%s\n" % (row[0],row[1],row[2]))
-
-    outfile.write("END")
-    outfile.close()
+    epoch = datetime.utcfromtimestamp(0)
+    num_hosts, num_empty_hosts = 0, 0
+    htmlfile = open("/var/www/html/time-data/index.html", 'w')
+    htmlfile.write("<!DOCTYPE html>\n\n<html>\n")
+    htmlfile.write("<head>\n\t<link rel='stylesheet' href='style.css'>\n</head>\n")
+    tablesize, timessize, datedsize = get_sizes(conn)
+    num_images = len(os.listdir(img_dir))
+    htmlfile.write("<p style='font-size:35px'>Rows in 'times': %s</p>" % timessize)
+    htmlfile.write("<p style='font-size:35px'>Rows in 'dated': %s</p>" % datedsize)
+    htmlfile.write("<p style='font-size:35px'>Database size: %s MB</p>" % tablesize)
     
-    with open("timeseries.txt", 'r') as infile:
-        epoch = datetime.utcfromtimestamp(0)
-        hostname = ""
+    for host in sorted_hosts:
         points = []
-        num_hosts, num_empty_hosts = 0, 0
-        for line in infile:
-            line = line.strip()
-            if re.search('[a-zA-Z]', line) != None:
-                if len(points) > 0:
-                    num_hosts += 1
-                    epochtimes, times, offsets = zip(*sorted(points, key=operator.itemgetter(0)))
-                    plt.clf()
-                    plt.gca().xaxis.set_major_formatter(mdt.DateFormatter('%m/%d/%Y'))
-                    plt.gca().xaxis.set_major_locator(mdt.DayLocator())
-                    plt.plot(times, offsets)
-                    plt.gcf().autofmt_xdate()
-                    plt.savefig(savefolder+hostname.replace(".","-")+".png")
-                else:
-                    num_empty_hosts += 1
-                hostname = line
-                points = []
-            else:
-                row = line.split(',')
-                time = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
-                epochseconds = int((time-epoch).total_seconds())
-                points.append((epochseconds, time, int(row[2])))
-        print("Hosts with no 'times' entry: " + str(num_empty_hosts))
-        print("Images created: " + str(num_hosts))
-
-
+        c.execute("%swhere host='%s'" % (cmd, host))
+        for row in c.fetchall():
+            time, ipaddr, offset = row
+            offset = int(offset)
+            if offset < 3:
+                offset = 0
+#            time = datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
+            epochseconds = int((time-epoch).total_seconds())
+            points.append((epochseconds, time, int(offset)))
+        abs_sum = 0
+        for es, t, oset in points:
+            abs_sum += abs(oset)
+        if abs_sum == 0:
+            num_empty_hosts += 1
+        else:
+            num_hosts += 1
+            epochtimes, times, offsets = zip(*sorted(points, key=operator.itemgetter(0)))
+            img_name = host.replace(".", "-")+".png"
+            plt.clf()
+            plt.gca().xaxis.set_major_formatter(mdt.DateFormatter('%m/%d/%Y'))
+            plt.gca().xaxis.set_major_locator(mdt.DayLocator())
+            plt.plot(times, offsets, "k-x")
+            plt.gcf().autofmt_xdate()
+            plt.savefig(img_dir+img_name)
+            htmlfile.write("<div class='floated_img'>\n\t<img src='%s' alt=\%s\>\n\t<p style='font-size:20px'>%s</p>\n</div>\n" % (img_dir.split("/")[-2:][0]+'/'+img_name, "timeseries plot " + str(num_hosts), img_name[:-4]))
+    
+    htmlfile.write("</html>")
+    htmlfile.close()
+    print ("Hosts with no offset: " + str(num_empty_hosts))
+    print ("Images created: " + str(num_hosts))
+    
 def get_sizes(conn):
     c = conn.cursor()
     c.execute("select table_schema 'DB Name', SUM(data_length+index_length)/1024/1024 'Database Size' FROM information_schema.TABLES where table_schema='time'")
@@ -74,32 +79,10 @@ def get_sizes(conn):
         sizes.append(c.fetchall()[0][0])
     return (tablesize, sizes[0], sizes[1])
 
-def gen_html(conn, img_dir):
-    htmlfile = open("/var/www/html/time-data/index.html", 'w')
-    htmlfile.write("<!DOCTYPE html>\n\n<html>\n")
-    htmlfile.write("<head>\n\t<link rel=\"stylesheet\" href=\"style.css\">\n</head>\n")
-    tablesize, timessize, datedsize = get_sizes(conn)
-    num_images = len(os.listdir(img_dir))
-    htmlfile.write("<p style='font-size:35px'>Num plots: %s</p>" % str(num_images))
-    htmlfile.write("<p style='font-size:35px'>Rows in 'times': %s</p>" % timessize) 
-    htmlfile.write("<p style='font-size:35px'>Rows in 'dated': %s</p>" % datedsize)
-    htmlfile.write("<p style='font-size:35px'>Database size: %s MB</p>" % tablesize)
-    count = 1
-    folder = "plots"
-    for file in os.listdir(img_dir):
-        if "png" in file:
-            htmlfile.write("<div class=\"floated_img\">\n\t<img src=\"%s\" alt=\"%s\">\n\t<p>%s</p>\n</div>\n" %
-                           (folder+'/'+file, "timeseries plot "+str(count), file[:-4]))
-        count += 1
-    htmlfile.write("</html>")
-    htmlfile.close()
-
 if __name__ == "__main__":
-    conn = mysql_connect()
     if len(sys.argv) < 2:
-        print "Password Required"
+        print ("Password Required")
     else:
         conn = mysql_connect(sys.argv[1])
         dr = ("/var/www/html/time-data/plots")
         create_timeseries(conn, "/var/www/html/time-data/plots/")
-        gen_html(conn, dr)
